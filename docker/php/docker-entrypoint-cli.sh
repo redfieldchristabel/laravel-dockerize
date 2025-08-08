d70ffc5a72fb79f9455a92877215b2e4dacd053d
redfieldchristabel <epoolr88@gmail.com>
2025-08-09 00:47:32 +0800

    feat: improve entrypoint script for tail and composer install in prod and dev

--- docker/php/docker-entrypoint-cli.sh
+++ docker/php/docker-entrypoint-cli.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 set -e
 
-# Run composer install
-composer install
+# Check if the container is running in a development environment
+# In development, we include dev dependencies like testing and debugging tools
+if [[ "$DOCKER_ENV" == "development" ]]; then
+  echo "Running composer install with development dependencies..."
+  composer install
+else
+  # In production, we exclude dev dependencies and optimize the autoloader for performance
+  echo "Running composer install without dev dependencies and with optimized autoloader..."
+  composer install --no-dev --optimize-autoloader
+fi
 
 # Run php artisan optimize, output to Docker logs
+php artisan optimize:clear
 php artisan optimize
 
 # Check if this is an Artisan command container
@@ -13,4 +22,4 @@ if [[ "$1" == "php" && "$2" == "artisan" ]]; then
 fi
 
 # Stream Laravel logs to Docker logs as main process
-exec tail -f /var/www/storage/logs/*.log
+exec tail -n 0 -f /var/www/storage/logs/*.log
