d10604d3ff452fc5e69c60f9060e4a48c6f2b957
redfieldchristabel <epoolr88@gmail.com>
2026-01-22 18:03:20 +0800

    remove watcher from dev because need node to use this feature

--- docker/php/docker-entrypoint-octane-swoole.sh
+++ docker/php/docker-entrypoint-octane-swoole.sh
@@ -18,12 +18,5 @@ php artisan optimize
 # Stream Laravel logs to Docker logs in background
 tail -n 0 -f /var/www/storage/logs/*.log &
 
-
-
-if [[ "$DOCKER_ENV" == "development" ]]; then
-   # Start Laravel Octane with Swoole as the main process
-  exec php artisan octane:start --server=swoole --host=0.0.0.0 --port=8000 --watch
-else
-  # Start Laravel Octane with Swoole as the main process
-  exec php artisan octane:start --server=swoole --host=0.0.0.0 --port=8000
-fi
+# Start Laravel Octane with Swoole as the main process
+exec php artisan octane:start --server=swoole --host=0.0.0.0 --port=8000
