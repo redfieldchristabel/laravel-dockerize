d2eb752abe775a7603bc9376e0f7850af822c08e
redfieldchristabel <epoolr88@gmail.com>
2026-03-15 08:10:06 +0800

    chore: add Laravel project validation to scaffold command

--- cli/lib/commands/scaffold.dart
+++ cli/lib/commands/scaffold.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:args/command_runner.dart';
 import 'package:logging/logging.dart';
 import '../utils/wizard.dart';
@@ -13,15 +14,22 @@ class ScaffoldCommand extends Command {
 
   @override
   void run() {
-    _log.info('🐳 Welcome to the Laravel Dockerize Scaffolder!');
+    _log.info('🐋 Welcome to the Laravel Dockerize Scaffolder!');
     _log.info('----------------------------------------------');
+
+    // Check if current directory is a Laravel project
+    if (!File('artisan').existsSync() || !Directory('app').existsSync()) {
+      _log.severe(
+          'Error: This is not a Laravel project. Please run in a Laravel project directory (must contain "artisan" and "app/").');
+      exit(1);
+    }
 
     final wizard = ScaffoldWizard();
     final options = wizard.run();
 
     _log.info('\n✅ Scaffolding project with PHP ${options.phpVersion}...');
 
-    //
+    // Further logic for file generation will go here
 
     _log.fine('Finished wizard process.');
   }
@@ -54,41 +62,41 @@ class ScaffoldOptions {
 class ScaffoldWizard extends Wizard<ScaffoldOptions> {
   @override
   List<WizardStep> get steps => [
-    SelectionStep(
-      id: 'php_version',
-      label: 'PHP Version',
-      question: 'Select PHP Version:',
-      options: ['8.1', '8.2', '8.3', '8.4'],
-    ),
-    ConfirmStep(
-      id: 'use_octane',
-      label: 'Use Octane',
-      question: 'Do you want to use Laravel Octane?',
-    ),
-    ConfirmStep(
-      id: 'is_filament',
-      label: 'Filament Project',
-      question: 'Is this a Filament project?',
-    ),
-    EnumSelectionStep<Database>(
-      id: 'database',
-      label: 'Database',
-      question: 'Select Database:',
-      options: Database.values,
-    ),
-    EnumSelectionStep<WebSocketTech>(
-      id: 'web_socket',
-      label: 'Web Socket Technology',
-      question: 'Select WebSocket Technology:',
-      options: WebSocketTech.values,
-    ),
-    EnumSelectionStep<BaseImage>(
-      id: 'base_image',
-      label: 'Base Image',
-      question: 'Which base docker image do you want to use?',
-      options: BaseImage.values,
-    ),
-  ];
+        SelectionStep(
+          id: 'php_version',
+          label: 'PHP Version',
+          question: 'Select PHP Version:',
+          options: ['8.1', '8.2', '8.3', '8.4'],
+        ),
+        ConfirmStep(
+          id: 'use_octane',
+          label: 'Use Octane',
+          question: 'Do you want to use Laravel Octane?',
+        ),
+        ConfirmStep(
+          id: 'is_filament',
+          label: 'Filament Project',
+          question: 'Is this a Filament project?',
+        ),
+        EnumSelectionStep<Database>(
+          id: 'database',
+          label: 'Database',
+          question: 'Select Database:',
+          options: Database.values,
+        ),
+        EnumSelectionStep<WebSocketTech>(
+          id: 'web_socket',
+          label: 'Web Socket Technology',
+          question: 'Select WebSocket Technology:',
+          options: WebSocketTech.values,
+        ),
+        EnumSelectionStep<BaseImage>(
+          id: 'base_image',
+          label: 'Base Image',
+          question: 'Which base docker image do you want to use?',
+          options: BaseImage.values,
+        ),
+      ];
 
   @override
   ScaffoldOptions build(Map<String, dynamic> answers) {
